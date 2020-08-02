9cc2844a0e537f6b2bafb24aeed057518c4ea94d
iEuler <48609238+iEuler@users.noreply.github.com>
2020-08-01 23:23:12 -0400

    add q6

---
+++ q0006.py
@@ -0,0 +1,47 @@
+"""
+6. ZigZag Conversion
+https://leetcode.com/problems/zigzag-conversion/
+
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+P   A   H   N
+A P L S I I G
+Y   I   R
+And then read line by line: "PAHNAPLSIIGYIR"
+
+Write the code that will take a string and make this conversion given a number of rows:
+
+string convert(string s, int numRows);
+Example 1:
+
+Input: s = "PAYPALISHIRING", numRows = 3
+Output: "PAHNAPLSIIGYIR"
+Example 2:
+
+Input: s = "PAYPALISHIRING", numRows = 4
+Output: "PINALSIGYAHRPI"
+Explanation:
+
+P     I    N
+A   L S  I G
+Y A   H R
+P     I
+"""
+
+
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1:
+            return s
+        ans = ['']*numRows
+        r, dr = 0, 1
+        for ch in s:
+            ans[r] += ch
+            r += dr
+            if r == numRows-1 or r == 0:
+                dr *= -1
+        return ''.join(ans)
+
+s = "PAYPALISHIRING"
+numRows = 4
+print(Solution().convert(s,numRows))
